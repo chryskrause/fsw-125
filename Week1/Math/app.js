55b6219d1d246d55ad21871acd255a44f5574b0a
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2020-04-12 22:03:49 -0500

    Node Module Basics and Patterns

---
+++ Week1/Math/app.js
@@ -0,0 +1,6 @@
+const { sum, subtract, multiply, divide } = require("./math.js")
+
+console.log(sum(14,36))
+console.log(subtract(100, 50))
+console.log(multiply(15, 15))
+console.log(divide(144, 12))
