e1724ddee3d5152aac4f1ebf47f28ee1d4a61e52
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2020-05-04 16:16:55 -0500

    Bounty Hunter Part 1. Working in Postman

---
+++ Week3/BountyHunter/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+
+app.use(express.json())
+app.use("/bounty", require("./routes/bountyRoute.js"))
+
+app.listen(3005, ()=> {
+    console.log("Server is waiting on port 3005")
+})
