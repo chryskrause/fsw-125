5794c4564444d89485c4e540281d9eb50483b4ba
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2020-08-02 15:43:06 -0500

    Math assignment

--- Week1/Math/app.js
+++ Week1/Math/app.js
@@ -1,6 +1,21 @@
-const { sum, subtract, multiply, divide } = require("./math.js")
+const {sum, subtract, multiply, divide} = require("./math")
 
-console.log(sum(14,36))
-console.log(subtract(100, 50))
-console.log(multiply(15, 15))
-console.log(divide(144, 12))
+console.log(sum(43, 57))
+console.log(subtract(1000, 900))
+console.log(multiply(2, 50))
+console.log(divide(950, 9.5))
+
+console.log(sum(45, 90))
+console.log(subtract(7, 9))
+console.log(multiply(20, 10))
+console.log(divide(50, 5))
+
+console.log(sum(200, 3000))
+console.log(subtract(222, 111))
+console.log(multiply(15, 20))
+console.log(divide(100, 4))
+
+console.log(sum(23, 23))
+console.log(subtract(500, 250))
+console.log(multiply(21, 50))
+console.log(divide(1400, 20))
